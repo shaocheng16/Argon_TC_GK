4d9d5041af1d21203593ada4d9bfc32df7233ce4
cluster k048900 <shaocheng16@gmail.com>
2020-04-20 17:32:37 +0900

    update scripts

--- run_seeds.sh
+++ run_seeds.sh
@@ -17,16 +17,20 @@ module load intel-mpi/5.1.3.258
 #module load pgi
 EXE=~/lmp_mpi
 
+lmp_input=in.demo
+lmp_dat=PbS_bulk.d
+
 for i in {1..10} ; do
 
   dir_name="seed${i}"
   mkdir $dir_name
   v_seed="$i$i$i"
-  cp Ar_structure.lmp in.GK  $dir_name
+  cp  ${lmp_input} ${lmp_dat} $dir_name
   cd $dir_name
   pwd
-  sed -i "s/variable seed.*/variable seed equal $v_seed/g" in.GK
-  mpijob   $EXE < in.GK > out
+  sed -i "s/variable seed.*/variable seed equal $v_seed/g" ${lmp_input}
+  mpijob   $EXE < ${lmp_input} > out
   cd ..
 done
 wait
+
